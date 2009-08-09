e56badb0d905f705780bc343de8b735fc5ce60e9
Jeremy Cowgar <jeremy@cowgar.com>
2009-08-09 13:44:16 +0000

    * Added user profile links to the main forum index * Added a query to optimize (a bit) the topic list. It's our slowest   page and could use some help.

--- sql/update_0003.sql
+++ sql/update_0003.sql
@@ -4,3 +4,9 @@
 
 ALTER TABLE messages ADD COLUMN last_edit_at datetime;
 UPDATE messages SET last_edit_at=created_at;
+
+--
+-- Index well for the topic list, our slowest part of the whole system
+-- 
+
+CREATE INDEX topic_list ON messages(last_post_at, parent_id);
