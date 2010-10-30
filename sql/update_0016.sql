eeb7f4ca8823f47b926786ef902d285326e0cbb1
Jeremy Cowgar <jeremy@cowgar.com>
2010-10-30 13:10:13 +0000

    Set the default charset for the wiki table to prevent collation errors when using unions. Also added a few indexes including a full text index on the wiki table.

--- sql/update_0016.sql
+++ sql/update_0016.sql
@@ -2,6 +2,7 @@
 -- Wiki system
 --
 
+DROP TABLE IF EXISTS wiki_page;
 CREATE TABLE wiki_page (
   rev integer not null default 0,
   name varchar(128) not null,
@@ -9,5 +10,8 @@ CREATE TABLE wiki_page (
   created_by_id integer not null references users(id),
   change_msg text not null,
   wiki_text text not null,
-  primary key (name,rev)
-);
+  PRIMARY KEY (name,rev),
+  KEY lookup (name,rev),
+  KEY recent (rev,created_at),
+  FULLTEXT KEY subject (name,wiki_text)
+) DEFAULT CHARSET=cp1251;
