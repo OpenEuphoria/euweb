e71445ac0f48ae04d33ffce4ee5f47725719d994
Jeremy Cowgar <jeremy@cowgar.com>
2010-11-06 14:17:51 +0000

    Strip X.Y.Z. section numbering from manual indexing. Also add in the Major Category name as a 'name' prefix to give the result some context.

--- source/manimport.ex
+++ source/manimport.ex
@@ -19,7 +19,7 @@ constant now = dt:now()
 
 procedure add(sequence fname, sequence a_name, sequence name, sequence content)
 	content = re:find_replace(re_tag, content, "")
-	edbi:execute("""INSERT INTO manual (created_at, filename, a_name, name, content) 
+	edbi:execute("""INSERT INTO manual (created_at, filename, a_name, name, content)
 		VALUES (%T, %s, %s, %s, %s)""", {
 			now, fname, a_name, name, content })
 end procedure
@@ -40,16 +40,16 @@ sequence files = cmds[3..$]
 for i = 1 to length(files) do
 	sequence fname = files[i]
 	sequence bfname = filename(fname)
-	sequence a_name = "", content = "", name = ""
-	
+	sequence a_name = "", content = "", name = "", maj_name = ""
+
 	printf(1, "processing %s\n", { fname })
 	sequence html = read_file(fname)
-	
+
 	sequence lines = seq:split(html, "\n")
 
 	for j = 1 to length(lines) do
 		sequence line = lines[j]
-	
+
 		if begins("<a name=\"", line) then
 			-- We only want h2, h3 and h4 a names
 			sequence nline = lines[j+1]
@@ -60,22 +60,29 @@ for i = 1 to length(files) do
 				if length(a_name) and length(content) then
 					add(bfname, a_name, name, content)
 				end if
-			
+
 				content = ""
-			
+
 				integer end_pos = find_from('"', line, 10)
 				if end_pos = 0 then
 					continue
 				end if
-			
+
 				a_name = line[10..end_pos-1]
-			
+
 				end_pos = find_from('<', nline, 5)
 				if end_pos = 0 then
 					continue
 				end if
-			
-				name = nline[5..end_pos - 1]
+
+				-- start_pos is after the 1.2.3.4 stuff
+				integer start_pos = find(' ', nline)
+				name = nline[start_pos + 1..end_pos - 1]
+				if begins("<h2", nline) then
+					maj_name = name
+				elsif length(maj_name) then
+					name = maj_name & ": " & name
+				end if
 			end if
 		elsif length(a_name) then
 			content &= line & "\n"
